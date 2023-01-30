b0a112ab306a8f81c4f9898651d605df04785558
Yeowon KIM <kkyoo152@gmail.com>
2023-01-30 15:38:06 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.09 ms, Memory: 69.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,19 @@
+class Solution {
+     public String solution(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        int index = 0;
+        for (char c : s.toCharArray()) {
+            if (c == ' ') {
+                index = 0;
+                sb.append(" ");
+            } else if (index++ % 2 == 0) {
+                sb.append(Character.toUpperCase(c));
+            } else {
+                sb.append(Character.toLowerCase(c));
+            }
+        }
+
+        return sb.toString();
+    }
+}
