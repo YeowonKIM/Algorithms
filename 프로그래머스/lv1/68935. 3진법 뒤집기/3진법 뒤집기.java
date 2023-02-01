ee308f3550e0e4eea6567338f7c0ea1e8dd23377
Yeowon KIM <kkyoo152@gmail.com>
2023-02-01 13:57:00 +0900

    [level 1] Title: 3진법 뒤집기, Time: 9.26 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,16 +1,6 @@
 class Solution {
-     public int solution(int n) {
-        String ternary = "";
-        while (n>0) {
-            ternary = ternary + ""+n%3;
-            n /= 3;
-        }
-        System.out.println("ternary="+ternary);
-        int sum =0;
-        for (int i=0; i<ternary.length(); i++) {
-            char ch = ternary.charAt(ternary.length()-(i+1));
-            sum += (ch -'0')*Math.pow(3, i);
-        }
-        return sum;
+    public int solution(int n) {
+        int answer = 0;
+        return answer;
     }
 }
