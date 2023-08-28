1866d8106ba0ad44dad22ea778dd20ec766714e4
Yeowon KIM <kkyoo152@gmail.com>
2023-08-28 15:06:21 +0900

    Time: 61 ms (89.55%), Space: 17.7 MB (13.84%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,22 +1,10 @@
 class Solution:
     def twoSum(self, nums, target):
-        origin = {}
-        
+        hashmap = {}
+
         for idx, val in enumerate(nums):
-            if val in origin:
-                origin[val].append(idx)
+            difference = target - val
+            if difference in hashmap:
+                return [hashmap[difference], idx]
             else:
-                origin[val] = [idx]
-        print(origin)
-            
-        nums.sort()
-        
-        left, right = 0, len(nums)-1
-        
-        while left < right:
-            if (nums[left] + nums[right]) < target:
-                left += 1
-            elif (nums[left] + nums[right]) > target:
-                right -= 1
-            else:
-                return [origin[nums[left]][0], origin[nums[right]][-1]]
+                hashmap[val] = idx
