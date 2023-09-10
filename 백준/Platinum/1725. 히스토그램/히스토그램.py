7eecdc1060bcd20cb9d9a64c5ce2377546f7d5d6
Yeowon KIM <kkyoo152@gmail.com>
2023-09-10 23:56:42 +0900

    [Platinum V] Title: 히스토그램, Time: 156 ms, Memory: 44084 KB -BaekjoonHub

--- 백준/Platinum/1725. 히스토그램/히스토그램.py
+++ 백준/Platinum/1725. 히스토그램/히스토그램.py
@@ -2,17 +2,17 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-list = [int(input()) for _ in range(N)]
+nums = [int(input()) for _ in range(N)]
 
 stack = []
 max_area = 0
 for i in range(N):
     idx = i
-    while stack and stack[-1][1] > list[i]:
+    while stack and stack[-1][1] > nums[i]:
         idx, height = stack.pop()
         area = (i-idx) * height
         max_area = max(max_area, area)
-    stack.append([idx, list[i]])
+    stack.append([idx, nums[i]])
 
 while stack:
     idx, height = stack.pop()
