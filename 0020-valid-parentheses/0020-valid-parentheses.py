9ae67fbb265077b267a28e692c0a92530e257153
Yeowon KIM <kkyoo152@gmail.com>
2023-09-13 12:28:02 +0900

    Time: 38 ms (67.19%), Space: 16.3 MB (49.79%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for bracket in s:
+            if bracket == "(":
+                stack.append(")")
+            elif bracket == "{":
+                stack.append("}")
+            elif bracket == "[":
+                stack.append("]")
+            elif not stack or stack.pop() != bracket:
+                return False
+        return not stack
+
